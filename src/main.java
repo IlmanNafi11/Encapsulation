3294cd89e3dde0b9da689f59afd0506f422f0aef
IlmanNafi11 <studyilmannafi@gmail.com>
2024-02-22 01:03:03 +0700

    get data product

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ import Kelas.*;
 
 public class main {
     public static void main(String[] args) throws Exception {
+
         // Membuat object login
         Login lg = new Login("ilmannafi", "ilman1234 ");
         // get username dan password
@@ -16,6 +17,7 @@ public class main {
         lg.setPassword("afi1234");
         System.out.println("Username: "+ lg.getUsername() +
         "\nPassword: "+ lg.getPassword());
+
         System.out.println("================================================================");
         // Membuat object DataPengguna
         DataPengguna dtPengguna = new DataPengguna("Ilman","331403558","081292332","Sragen","Owner");
@@ -31,8 +33,19 @@ public class main {
         // set/ubah noTlp
         dtPengguna.setTelephone("0852924534");
         System.out.println("No Hp: " + dtPengguna.getTelephone());
+
         System.out.println("================================================");
-        // membuat object product-
-        
+        // membuat object product
+        Product produk = new Product("Softcase", "ACS-123", "Aksesoris", 35000, 10, "Tri handoko", "SP-1321");
+        //get data product
+        System.out.println("Data Product: ");
+        System.out.println("Id Produk: " + produk.getId_produk() +
+        "\nKategori: " + produk.getKategori()+
+        "\nNama Product: "+ produk.getNama_Product()+
+        "\nStok: " + produk.getJumlah()+
+        "\nHarga: "+ produk.getHarga()+
+        "\nID-Supplier: "+ produk.getId_suplier()+
+        "\nNama Supplier: "+ produk.getNama_suplier());    
+            
     }
 }
